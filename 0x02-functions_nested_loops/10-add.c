7167d072d69c31cb7022fda64262db1460910b28
Béryl <beryl.houessou@gmail.com>
2022-06-07 15:39:07 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - adds two integers and returns the result
  * @a: the number to add
- * @b: the number to add 
+ * @b: the number to add
  * Return: int - Result of addition
  */
 int add(int a, int b)
