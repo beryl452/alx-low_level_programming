efa794d6163de993299803cb6f474739c5283468
Béryl <beryl.houessou@gmail.com>
2022-06-10 16:22:15 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** main - largest prime factor of the number 612852475143
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+long int x = 612852475143, y ;
+
+for (y = 1; y < x; y++)
+{
+	if (x % y == 0)
+		x = x / y;
+}
+	printf("%ld\n", y);
+	return (0);
+}
