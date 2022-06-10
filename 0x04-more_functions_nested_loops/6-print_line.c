c6c309c4fc349124716452da3bd7bc9cef53a10d
Béryl <beryl.houessou@gmail.com>
2022-06-10 14:37:35 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,12 @@ void print_line(int n)
 {
 	int i;
 
-if (n > 0)
-{
-for (i = 0; i < 0; i++)
-{
-	_putchar('_');
-}
-}
-_putchar('\n');
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
