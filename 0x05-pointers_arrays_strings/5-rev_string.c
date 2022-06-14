6fc524891ee16ccc32f0feecdd6dfecafeae43e2
Béryl <beryl.houessou@gmail.com>
2022-06-14 12:37:34 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int  length, i;
+
+length = strlen(s);
+for (i = length - 1; i >= 0; i--)
+	printf("%c", *(s + i);
+}
