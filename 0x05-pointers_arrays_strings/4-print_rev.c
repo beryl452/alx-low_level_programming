68d60315d3cc0f936e97c57850a9072e9a4d6299
Béryl <beryl.houessou@gmail.com>
2022-06-14 00:46:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: the string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int length, i;
+
+length = strlen(s);
+for (i = (length - 1); i > 0; i--)
+	printf("%c", *(s + i));
+}
