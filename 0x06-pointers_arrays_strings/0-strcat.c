e80b0b89fabd3725bcdc6eaf31e6b2ea2fdbc712
Béryl <beryl.houessou@gmail.com>
2022-06-16 11:22:42 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,8 @@ char *_strcat(char *dest, char *src)
 	size_t dest_len = strlen(dest), i;
 
 	for (i = 0; src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+		dest[dest_len] = src[i];
+	dest[dest_len] = '\0';
 
 	return (dest);
 }
