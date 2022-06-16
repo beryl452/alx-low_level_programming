da0560452db55f750e1e5b9f21eb6ed8060b83bf
Béryl <beryl.houessou@gmail.com>
2022-06-16 11:16:24 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcat - that concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: char*
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = strlen(dest), i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
