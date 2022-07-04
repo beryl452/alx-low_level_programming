bba2ddf6f6db10ce8302b9d06de8701757ee62fb
Béryl <beryl.houessou@gmail.com>
2022-07-04 13:44:31 -0700

    1-int_dog.c

---
+++ 0x0E-structures_typedef/1-int_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - init a dog wit its struct
+ * @d: pointer to a dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: int a dog with its struct
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d = malloc(sizeof(struct dog));
+if (d != NULL)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - dog structure
  * @name: name
@@ -15,5 +18,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
