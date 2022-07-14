8ec4138ea63e8da17f538feada3b06da2be246fb
Béryl <beryl.houessou@gmail.com>
2022-07-14 13:43:01 -0700

    Fix bug in first Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,17 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
 	{
-		if (h->str == null)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
 			printf("[%i] %s\n", h->len, h->str);
 			h = h->next;
-			c++;
+			count++;
 		}
 	}
 	return (count);
