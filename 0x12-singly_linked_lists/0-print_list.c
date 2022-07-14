e4c5279b455fdd8bed62734fe061eb218ef7d682
Béryl <beryl.houessou@gmail.com>
2022-07-14 13:25:02 -0700

    first Task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list_t list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h == null)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+			h = h->next;
+			c++;
+		}
+	}
+	return (count);
+}
