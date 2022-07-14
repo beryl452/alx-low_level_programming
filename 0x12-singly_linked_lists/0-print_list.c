8c2ae5786c0c5a5190b643e52f83d433ec39d57c
Béryl <beryl.houessou@gmail.com>
2022-07-14 13:29:23 -0700

    first Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: head of the list
  * Return: the number of nodes
  */
