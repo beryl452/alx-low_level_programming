bd65e1517843fbaacc53dcf7e36234db28511707
Béryl <beryl.houessou@gmail.com>
2022-08-16 05:46:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
