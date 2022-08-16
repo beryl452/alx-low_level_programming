552df758480796747f5f8edf919730ac2e4be974
Béryl <beryl.houessou@gmail.com>
2022-08-16 13:00:00 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,9 +17,9 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_s;
+} listint_t;
 
-void _putchar(char c);:
+void _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
