f760649f4521b2dcfc263a78297f183e7d626d11
Béryl <beryl.houessou@gmail.com>
2022-08-17 13:31:35 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  * Return: 1 if succeeded, -1 if it failed
  */
 
-delete_nodeint_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *tmp, *tmp2;
